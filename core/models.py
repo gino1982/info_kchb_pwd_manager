65af343970449f1c42c088e08bb266a6e81c5365
gino1982 <gino19820915@gmail.com>
2026-04-17 09:44:44 +0800

    後台管理介面改為中文顯示

--- core/models.py
+++ core/models.py
@@ -11,6 +11,10 @@ class Employee(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = "員工"
+        verbose_name_plural = "員工"
 
 # 2. 系統表
 class SystemApp(models.Model):
@@ -20,6 +24,10 @@ class SystemApp(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "系統"
+        verbose_name_plural = "系統"
 
 # 3. 帳密配置表 (核心！)
 class Account(models.Model):
@@ -32,6 +40,8 @@ class Account(models.Model):
     class Meta:
         # 防呆機制：一人一系統只能有一組帳號！
         unique_together = ('employee', 'system')
+        verbose_name = "帳戶"
+        verbose_name_plural = "帳戶"
 
     def __str__(self):
         return f"{self.employee.name} 的 {self.system.name} 帳號"
