21f3aa07c405706e0b68ad1c69fa7bdd5f18fd4b
gino1982 <gino19820915@gmail.com>
2026-04-17 11:33:20 +0800

    將語系改成中文&新增Logo

--- config/settings.py
+++ config/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
 ]
 
 MIDDLEWARE = [
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from core import views  #  app的views.py，裡面有home函式
 
+admin.site.site_header = '高雄市政府衛生局_系統帳密管理'  # 左上角的「logo」
+admin.site.site_title = '衛生局帳密系統'                 # 瀏覽器標籤的title
+admin.site.index_title = '後台管理首頁'                  # 後台首頁的標題
+
 urlpatterns = [
     path('admin/', admin.site.urls), # 原本的後台urls
     path('', views.home, name='home'), #引號裡空白代表「首頁」
